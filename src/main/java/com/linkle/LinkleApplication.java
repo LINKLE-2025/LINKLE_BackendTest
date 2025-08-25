57d94828e3b67684e5af706362893377c00f8aba
Gaegulin <121297188+Gaegulin@users.noreply.github.com>
2025-08-25 17:16:41 +0900

    태그테스트할거에요

--- src/main/java/com/linkle/LinkleApplication.java
+++ src/main/java/com/linkle/LinkleApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LinkleApplication {
-
+	//태그테스트
     public static void main(String[] args) {
         SpringApplication.run(LinkleApplication.class, args);
     }
